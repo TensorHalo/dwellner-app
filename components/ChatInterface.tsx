cf514bbef3de6e1bb8cd13a6e8e19a4b33d8a653
difanj0713 <difanjiao@gmail.com>
2025-01-02 23:57:30 +0800

    user auth /w cognito

--- components/ChatInterface.tsx
+++ components/ChatInterface.tsx
@@ -3,7 +3,7 @@ import { View, Text, TextInput, TouchableOpacity, Image, KeyboardAvoidingView, P
 import React, { useState, useRef, useEffect } from 'react';
 import { Feather as FeatherIcon } from '@expo/vector-icons';
 import { Audio } from 'expo-av';
-import { ListingData } from '@/utils/firebase';
+import { ListingData } from '@/utils/listingData';
 import { useRouter } from 'expo-router';
 import ListingsButton from './ListingsButton';
 import { getCurrentSession } from '@/utils/cognitoConfig';
